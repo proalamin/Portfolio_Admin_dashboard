600dac97684732a4778c52aba62ed0ef63fe0578
proalamin <pro.alamin247@gmail.com>
2024-01-02 18:46:47 +0600

    side menu collapse functionality icon added

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,45 @@
+// import {
+//   ArrowLeftEndOnRectangleIcon,
+//   ArrowLeftIcon,
+//   ArrowRightIcon,
+//   HomeIcon,
+// } from "@heroicons/react/24/solid";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi2";
+import { useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 
 function Dashboard() {
+
+  const [menuCollapse, setMenuCollapse] = useState(false);
+  const menuIconClick = () => {
+    setMenuCollapse(!menuCollapse);
+  }
+
+
   return (
     <div className='h-full'>
-      <Sidebar className=' bg-red-200 h-lvh'>
-        <div >
-          <h1 className='text-center py-5 font-bold text-xl	'>Portfolio_Admin</h1>
+      <Sidebar className=' bg-red-200 h-lvh'
+        collapsed={menuCollapse}
+        collapsedWidth="100px"
+
+      >
+        <div className="flex items-center justify-center	">
+          <span className='text-center py-5 font-bold text-xl	'>
+            {menuCollapse ? ('Admin' ):'Portfolio_Admin'}
+          </span>
+          <span className="flex justify-end pt-2 mx-2 font-extrabold text-xl" onClick={menuIconClick}>
+            {
+              menuCollapse ? (
+                <HiChevronDoubleRight />
+              ) : (
+
+                <HiChevronDoubleLeft />
+              )
+            }
+          </span>
         </div>
+
         <Menu className='font-bold'>
           <MenuItem>Dashboard</MenuItem>
           <SubMenu className='' label="Projects">
