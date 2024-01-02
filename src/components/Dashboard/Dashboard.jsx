5df34e2c7264ff427ea7ff88b6eb626b25c77ed5
proalamin <pro.alamin247@gmail.com>
2024-01-02 18:58:37 +0600

    side menu logout button added and also collapseable condition added

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@
 //   ArrowRightIcon,
 //   HomeIcon,
 // } from "@heroicons/react/24/solid";
-import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi2";
+import { HiChevronDoubleLeft, HiChevronDoubleRight, HiChevronLeft } from "react-icons/hi2";
 import { useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
@@ -26,7 +26,7 @@ function Dashboard() {
       >
         <div className="flex items-center justify-center	">
           <span className='text-center py-5 font-bold text-xl	'>
-            {menuCollapse ? ('Admin' ):'Portfolio_Admin'}
+            {menuCollapse ? ('Admin') : 'Portfolio_Admin'}
           </span>
           <span className="flex justify-end pt-2 mx-2 font-extrabold text-xl" onClick={menuIconClick}>
             {
@@ -58,6 +58,17 @@ function Dashboard() {
           <MenuItem className=''>Calendar</MenuItem>
           <MenuItem>Profile</MenuItem>
         </Menu>
+
+        {/* <div className="flex justify-center">
+          <button className="btn btn-outline btn-error">Error</button>
+        </div> */}
+
+        <div className="text-center">
+          {menuCollapse ? (<button className="btn btn-outline btn-error btn-xs mt-5"><HiChevronLeft />
+            Log out</button>) :
+            (<button className="btn btn-outline btn-error btn-sm mt-5 "><HiChevronLeft />
+              Log out</button>)}
+        </div>
       </Sidebar>;
     </div>
   )
