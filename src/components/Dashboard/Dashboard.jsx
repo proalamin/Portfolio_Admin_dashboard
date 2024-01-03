43048822cf09fbf0ca1c23503352b34ab5cfe8c6
proalamin <pro.alamin247@gmail.com>
2024-01-03 13:09:25 +0600

    added side bar element icon and try sidebar more eye catching

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,15 @@
 //   ArrowRightIcon,
 //   HomeIcon,
 // } from "@heroicons/react/24/solid";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { GoProjectSymlink } from "react-icons/go";
+import { DiCodeigniter } from "react-icons/di";
+import { SiBloglovin } from "react-icons/si";
+import { RiContactsBookLine } from "react-icons/ri";
+import { SlCalender } from "react-icons/sl";
+import { CgProfile } from "react-icons/cg";
+import { IoMdHome } from "react-icons/io";
+
 import { HiChevronDoubleLeft, HiChevronDoubleRight, HiChevronLeft } from "react-icons/hi2";
 import { useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
@@ -21,12 +30,12 @@ function Dashboard() {
     <div className='h-full'>
       <Sidebar className=' bg-red-200 h-lvh'
         collapsed={menuCollapse}
-        collapsedWidth="100px"
+        collapsedWidth="85px"
 
       >
         <div className="flex items-center justify-center	">
           <span className='text-center py-5 font-bold text-xl	'>
-            {menuCollapse ? ('Admin') : 'Portfolio_Admin'}
+            {menuCollapse ? (<IoMdHome/>) : 'Portfolio_Admin'}
           </span>
           <span className="flex justify-end pt-2 mx-2 font-extrabold text-xl" onClick={menuIconClick}>
             {
@@ -40,23 +49,25 @@ function Dashboard() {
           </span>
         </div>
 
-        <Menu className='font-bold'>
-          <MenuItem>Dashboard</MenuItem>
-          <SubMenu className='' label="Projects">
+        <Menu className='font-bold border-double	rounded-md border-slate-400	m-2 bg-red-200'>
+          <MenuItem icon={<MdOutlineDashboardCustomize />}>Dashboard</MenuItem>
+          <SubMenu icon={<GoProjectSymlink />
+          } className='' label="Projects">
             <MenuItem> All Projects</MenuItem>
             <MenuItem>Add New Projects</MenuItem>
           </SubMenu>
-          <SubMenu className='' label="Skills">
-            <MenuItem>All Skills</MenuItem>
-            <MenuItem>Add New Skills</MenuItem>
+          <SubMenu icon={<DiCodeigniter />
+          } label="Skills">
+            <MenuItem icon={<MdOutlineDashboardCustomize />}>All Skills</MenuItem>
+            <MenuItem icon={<MdOutlineDashboardCustomize />}>Add New Skills</MenuItem>
           </SubMenu>
-          <SubMenu className='' label="Blogs">
-            <MenuItem>All Blogs</MenuItem>
+          <SubMenu icon={< SiBloglovin />} label="Blogs">
+            < MenuItem > All Blogs</MenuItem>
             <MenuItem>Add New Blogs</MenuItem>
           </SubMenu>
-          <MenuItem className=''>Manage Contacts</MenuItem>
-          <MenuItem className=''>Calendar</MenuItem>
-          <MenuItem>Profile</MenuItem>
+          <MenuItem icon={<RiContactsBookLine/>} >Manage Contacts</MenuItem>
+          <MenuItem icon={<SlCalender  />} >Calendar</MenuItem>
+          <MenuItem icon={<CgProfile />}>Profile</MenuItem>
         </Menu>
 
         {/* <div className="flex justify-center">
@@ -64,13 +75,13 @@ function Dashboard() {
         </div> */}
 
         <div className="text-center">
-          {menuCollapse ? (<button className="btn btn-outline btn-error btn-xs mt-5"><HiChevronLeft />
+          {menuCollapse ? (<button className="btn btn-outline btn-error btn-xs text-xs mt-5"><HiChevronLeft />
             Log out</button>) :
             (<button className="btn btn-outline btn-error btn-sm mt-5 "><HiChevronLeft />
               Log out</button>)}
         </div>
-      </Sidebar>;
-    </div>
+      </Sidebar >;
+    </div >
   )
 }
 
